29a534c960ad833c0b93916e81f75b77d221f2f7
coderman64 <coderman64@users.noreply.github.com>
2017-02-21 14:50:28 -0500

    Update bounce.js

--- bounce.js
+++ bounce.js
@@ -1,6 +1,5 @@
 var canvi = document.getElementById("myCanvas");
 var c = canvi.getContext("2d");
-var interval = window.setinterval(mainLoop, 17);
 
 window.addEventListener("touchstart", function(e){
   e.preventDefault();
@@ -10,9 +9,11 @@ window.addEventListener("touchmove", function(e){
   e.preventDefault();
 });
 
+var interval = window.setinterval(mainLoop, 17);
+
 function mainLoop(){
   c.fillStyle = "#000000";
   c.fillRect(0,0,500,500);
   c.fillStyle = "#FFFFFF";
-  c.fillRect(20,20,5,50);
+  c.fillRect(20,20,50,50);
 }
