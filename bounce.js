8aad3f01250d0f57f2d785e438430a5a4e23ba55
coderman64 <coderman64@users.noreply.github.com>
2017-02-22 08:07:32 -0500

    Update bounce.js

--- bounce.js
+++ bounce.js
@@ -4,6 +4,8 @@ var c = canvi.getContext("2d");
 canvi.height = window.innerHeight;
 canvi.width = window.innerWidth;
 
+var scene = "mainMenu";
+
 window.addEventListener("touchstart", function(e){
   e.preventDefault();
 });
@@ -14,9 +16,20 @@ window.addEventListener("touchmove", function(e){
 
 var interval = window.setInterval(mainLoop, 17);
 
+function mainMenu(){//the main loop of the main menu
+  c.fillStyle = "#FFFFFF";
+  c.font = "20px sans";
+  c.fillText(canvi.width/2,100,"Bounce");
+}
+
 function mainLoop(){
   c.fillStyle = "#000000";
   c.fillRect(0,0,canvi.width,canvi.height);
-  c.fillStyle = "#FFFFFF";
-  c.fillRect(20,20,50,50);
+  //c.fillStyle = "#FFFFFF";
+  //c.fillRect(20,20,50,50);
+  switch(scene){
+    case "mainMenu":
+      mainMenu();
+      break;
+  }
 }
