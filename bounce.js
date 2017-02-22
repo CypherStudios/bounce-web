8235f6d984c0cf06346a00ec39e6f0bc71a1816a
coderman64 <coderman64@users.noreply.github.com>
2017-02-22 09:31:54 -0500

    Update bounce.js

--- bounce.js
+++ bounce.js
@@ -4,13 +4,31 @@ var c = canvi.getContext("2d");
 canvi.height = window.innerHeight;
 canvi.width = window.innerWidth;
 
-var scene = "mainMenu";
+var currentScreen = "play";
+var touchList = [];
+
+var player1 = {
+  score: 0;
+  y: 0;
+};
+
+var player2 = {
+  score: 0;
+  y: 0;
+};
 
 window.addEventListener("touchstart", function(e){
+  touchList = e.touches;
   e.preventDefault();
 });
 
 window.addEventListener("touchmove", function(e){
+  touchList = e.touches;
+  e.preventDefault();
+});
+
+window.addEventListener("touchend", function(e){
+  touchList = e.touches;
   e.preventDefault();
 });
 
@@ -22,14 +40,38 @@ function mainMenu(){//the main loop of the main menu
   c.fillText(canvi.width/2,100,"Bounce");
 }
 
+function play(){
+  c.fillStyle = "#FFFFFF";
+  c.fillRect(50, player1.y-30, 10, 60);
+  c.fillRect(canvi.width-50, player2.y-30, 10, 60);
+  var leftTotal = 0;
+  var rightTotal = 0;
+  var leftCount = 0;
+  var rightCount = 0;
+  for(var i = 0; i < touchList.length; i++){
+    if(touchList[i].clientX < canvi.width/2){
+      leftTotal += touchList[i].clientY;
+      leftCount++;
+    }else{
+      rightTotal += touchList[i].clientY
+      rightCount++;
+    }
+  }
+  player1.y = leftTotal/leftCount;
+  player2.y = rightTotal/rightCount;
+}
+
 function mainLoop(){
   c.fillStyle = "#000000";
   c.fillRect(0,0,canvi.width,canvi.height);
   //c.fillStyle = "#FFFFFF";
   //c.fillRect(20,20,50,50);
-  switch(scene){
+  switch(currentScreen){
     case "mainMenu":
       mainMenu();
+      break;
+    case "play":
+      play();
       break;
   }
 }
